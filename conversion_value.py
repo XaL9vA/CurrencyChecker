9e01a2ad3a62c8fe37c367152d043d11880350d1
Sam <svsposobin@gmail.com>
2025-02-13 23:36:41 +0500

    MEN-3-Currecy-receiver [Closed Review #3]

--- conversion_value.py
+++ conversion_value.py
@@ -1,12 +1,12 @@
 from typing import Dict
-from config import config
+import config
 import requests
 import json
 
 
 class CurrenciesConverter:
     def __init__(self) -> None:
-        self.__API_KEY = config.api_key
+        self.__API_KEY: str = config.Config.api_key
 
     def convert(self, currency_from: str, currency_to: str, conversion_date: str) -> float:
         conversion_date: str = self.__correct_date_format(conversion_date=conversion_date)
@@ -18,15 +18,20 @@ class CurrenciesConverter:
         }
         with requests.Session() as local_session:
             try:
-                response: requests.Response = local_session.get("https://app.freecurrencyapi.com/dashboard")
+                response: requests.Response = local_session.get(
+                    "https://app.freecurrencyapi.com/dashboard",
+                    timeout=3)
                 if response.ok:
                     api_response: requests.Response = local_session.get(
                         "https://api.freecurrencyapi.com/v1/historical",
+                        timeout=3,
                         params=params
                     )
                     return json.loads(api_response.text)["data"][f"{conversion_date}"][f"{currency_to}"]
                 else:
                     print(f"Error - {response.status_code}")
+            except requests.exceptions.Timeout:
+                print("Connection error, try again later")
             except requests.exceptions.RequestException as exception:
                 print(f"Oops, something went wrong: {exception}")
 
