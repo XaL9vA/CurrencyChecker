a1f3d4e771c24305bdf38b57f0c95eb5256ce016
Sam <svsposobin@gmail.com>
2025-02-19 13:15:58 +0500

    MEN-4-Database [Addition]

--- tests/integration/test_conversion_value.py
+++ tests/integration/test_conversion_value.py
@@ -2,7 +2,7 @@ from typing import Optional
 from contextlib import nullcontext as does_not_raise
 import pytest
 from conversion_value import CurrenciesConverter
-
+from config import config
 
 class TestReceiptConversionValue:
     @pytest.mark.parametrize("currency_from, currency_to, conversion_date ,expected_result, expectation",
@@ -19,7 +19,7 @@ class TestReceiptConversionValue:
             expectation
     ) -> None:
         with expectation:
-            currency_converter = CurrenciesConverter()
+            currency_converter = CurrenciesConverter(api_key=config.api_key)
             assert currency_converter.convert(currency_from=currency_from,
                                               currency_to=currency_to,
                                               conversion_date=conversion_date
